6be6a02fb8b53ca86d4ee74daea83188517eee78
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2020-04-08 20:37:36 +0800

    ?

--- 6.sh
+++ 6.sh
@@ -85,13 +85,13 @@ git clone --depth=1 https://github.com/Boos4721/AnyKernel3.git  /drone/$NAME
 	zip -r $NAME-$VER.zip *
 git clone --depth=1 https://github.com/Boos4721/updater.git -b Kernel /drone/$WORK
     rm -rf ~/*.zip && rm -rf ~/drone/$WORK/*.zip
-    mv /drone/$NAME/$NAME-$VER.zip /drone/$WORK/$NAME-$VER.zip 
+    mv /drone/$NAME/$NAME-$VER.zip /drone/$WORK/$NAME-$VER.zip
     cd /drone/$WORK 
-    git add . && git commit -s -m "? "
     git remote remove origin
-    git remote add origin https://username:token@github.com/https://github.com/Boos4721/updater.git
+    git remote add origin https://username:token@github.com/Boos4721/updater.git
+    git add . && git commit -s -m "? "
     git push -uf origin Kernel
-
+    
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
