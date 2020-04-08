ff1a25ef0991fc6b7007ab1fd1d6a3a95083a4e7
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2020-04-09 01:35:38 +0800

    ！

--- 6.sh
+++ 6.sh
@@ -75,7 +75,7 @@ compile
 ############################################################
     cd  /drone/$NAME
     zip -r $NAME-$VER.zip *
-    git clone --depth=1 https://github.com/Boos4721/updater.git -b Kernel /drone/$WORK
+    git clone --depth=1 https://github.com/Boos4721/updater.git -b Kernel /drone/$WORK/$NAME
     mv /drone/$NAME/$NAME-$VER.zip /drone/$WORK/$NAME/$NAME-$VER.zip 
     cd /drone/$WORK
     git remote remove origin && git remote add origin https://$gayhub_username:%token@github.com/Boos4721/updater.git
