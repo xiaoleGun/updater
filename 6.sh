e74e84bf6c4260bd2c22455fdf729d253fa96639
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2020-04-08 20:10:48 +0800

    ？

--- 6.sh
+++ 6.sh
@@ -86,7 +86,8 @@ git clone --depth=1 https://github.com/Boos4721/AnyKernel3.git  /drone/$NAME
 git clone --depth=1 https://github.com/Boos4721/updater.git -b Kernel /drone/$WORK
     rm -rf ~/*.zip && rm -rf ~/drone/$WORK/*.zip
     mv /drone/$NAME/$NAME-$VER.zip /drone/$WORK/$NAME-$VER.zip 
-    cd /drone/$WORK && git add . && git commit -s -m "? "
+    cd /drone/$WORK 
+    git add . && git commit -s -m "? "
     git remote remove origin
     git remote add origin https://Boos4721:83b1bb59013786f09e3904036584b20d60747bfdn@github.com/https://github.com/Boos4721/updater.git
     git push -uf origin Kernel
