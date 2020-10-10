4650cd1a13eeebc3b20ef1d81a8eb3a707bd428d
xiaoleGun <53204756+xiaoleGun@users.noreply.github.com>
2020-10-10 19:45:21 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -8,10 +8,10 @@
 echo "Enable SSH ...."
 # sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config; 
-echo "AllowUsers root admin boos " >>  /etc/ssh/sshd_config
-echo "Patch Boos User...."
-sudo adduser boos && sudo passwd boos
+echo "AllowUsers root admin xiaolegun " >>  /etc/ssh/sshd_config
+echo "Patch xiaolegun User...."
+sudo adduser xiaolegun && sudo passwd 20071101
 echo "Patch Boos To Root User…."
-usermod -aG sudo boos
+usermod -aG sudo xiaolegun
 sudo /sbin/service sshd restart
 echo "Done ...."
